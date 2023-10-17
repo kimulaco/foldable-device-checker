1991d6f19ffac1470613111307530ebdf58d08bb
kimulaco <kimulaco@gmail.com>
2023-10-17 23:47:50 +0900

    Add eslint and prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  vueIndentScriptAndStyle: true,
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/eslint-config-prettier',
+  ],
+};
